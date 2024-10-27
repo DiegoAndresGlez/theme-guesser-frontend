0cbf759d98c2ed90c2f5ea78d5655e8bcdf918ec
DiegoAndresGlez <diegoglez077@gmail.com>
2024-10-27 13:18:26 -0700

    feat: routing added and tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,32 @@ import InfoComponent from './InfoComponent'
 import Login from './Login.jsx'
 import SignUp from './SignUp.jsx'
 import EditProfile from './EditProfile.jsx'
+// import EmailVerificationHandler from './EmailVerificationHandler.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
-	const [color, setColor] = useState('black');
-	const [brushSize, setBrushSize] = useState(10);
-
 	return (
-		<>
+		<BrowserRouter>
 			<Header/>
-			<Login/>
-			{/* <SignUp/> */}
-			{/* <EditProfile/> */}
-			{/* <CanvasComponent color={color} brushSize={brushSize} />
-			<Button id="change_to_white" onClick={() => setColor('white')}>White</Button>
-			<Button id="change_to_black" onClick={() => setColor('black')}>Black</Button>
-			<Button id="small_brush" onClick={() => setBrushSize(1)}>Small</Button>
-			<Button id="medium_brush" onClick={() => setBrushSize(5)}>Medium</Button>
-			<Button id="large_brush" onClick={() => setBrushSize(10)}>Large</Button> */}
-		</>
+			<main>			
+				<Routes>
+					{/* <Route path="/" element={<EmailVerificationHandler/>} /> */}
+					<Route path="/login" element={<Login/>}/>
+					<Route path="/signup" element={<SignUp/>}/>
+					<Route path="/edit-profile" element={<EditProfile/>}/>
+				</Routes>
+			</main>
+		</BrowserRouter>
 	)
 }
   
 export default App
+
+/*{ <CanvasComponent color={color} brushSize={brushSize} />
+<Button id="change_to_white" onClick={() => setColor('white')}>White</Button>
+<Button id="change_to_black" onClick={() => setColor('black')}>Black</Button>
+<Button id="small_brush" onClick={() => setBrushSize(1)}>Small</Button>
+<Button id="medium_brush" onClick={() => setBrushSize(5)}>Medium</Button>
+<Button id="large_brush" onClick={() => setBrushSize(10)}>Large</Button> }*/
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
+// tailwind.config.js
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        darkBlue: '#050c9c',
-      }
-    },
-  },
-  plugins: [],
+export const content = [
+  // ...
+  // make sure it's pointing to the ROOT node_module
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
 }
+
+export const darkMode = "class";
+export const plugins = [nextui()];
